ce9f1c215e84bafe22f8e6b7210100d7c0309276
Robert <robelix@gmx.de>
2022-07-26 12:24:23 +0200

    Funktion und Ausgabe

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,8 +4,18 @@ public class App {
     
     public static void main(String[] args) {
     
+// Objekte der angelegten klassen bilden
 
-       
+        Rectangle rectangle = new Rectangle(10.0, 10.0);
+
+        Triangle triangle = new Triangle(10.0, 10.0);
+
+        Circle circle = new Circle(5.0);
+
+        output("---------- Fläche ----------");
+        output("Fläche Rechteck: " + rectangle.area());
+        output("Fläche Dreieck: " + triangle.area());
+        output("Fläche Kreis: " + circle.area());
 
     }
 
